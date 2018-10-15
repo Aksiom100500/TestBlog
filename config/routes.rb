ef05df18ae481c22e1ac48f60041216a21433fa6
Pasha <Aksiom100500@gmail.com>
2018-10-15 19:34:55 +0300

    fix empty_lines and spaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   resources :categories do
     resources :comments, only: [:create]
-    # resources :articles, only: []
   end
   resources :articles, except: [:index] do
     resources :comments, only: [:create]
   end
   root 'categories#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
